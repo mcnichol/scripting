2541a4d68a3263256057899e2e6905c6e3347e51
Michael McNichol <mcnichol.m@gmail.com>
2018-05-22 00:28:06 -0500

    Validation for opapi

--- opsman/opapi.sh
+++ opsman/opapi.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
+set -e
+
+function guard_clause {
+    fail_script=false
+
+    [ -z "$1" ] && { echo "Missing: Provide OPSMAN_DOMAIN"; fail_script=true; }
+    [ -z "$2" ] && { echo "Missing: Provide OPSMAN_USER"; fail_script=true; }
+    [ -z "$3" ] && { echo "Missing: Provide OPSMAN_PASS"; fail_script=true; }
+
+    if $fail_script; then
+        exit 1;
+    fi
+}
+
 echo "OPsman API"
 
-case "$1" in 
+case "$1" in
     auth)
-        echo "Authenticating against $OPSMAN_DOMAIN"
+        guard_clause "$OPSMAN_DOMAIN" "$OPSMAN_USER" "$OPSMAN_PASS"
+
+        echo "Authenticating against $OPSMAN_DOMAIN domain with user $OPSMAN_USER"
         curl -s -k -H "Accept: application/json;charset=utf-8" -d "grant_type=password" -d "username=$OPSMAN_USER" -d "password=$OPSMAN_PASS" -u 'opsman:' https://$OPSMAN_DOMAIN/uaa/oauth/token | jq
         ;;
-    *) 
-        echo "Nothing to do"
+    *)
+        echo "Usage Instructions:"
+        echo " > ./opapi auth"
         exit 0
         ;;
 esac
 
 exit 0;
+
+
