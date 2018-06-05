ba9971c5b2e226e70374df56b7586a520680a391
Michael McNichol <mcnichol.m@gmail.com>
2018-06-05 11:20:29 -0500

    SED replace values in URLs

--- operating-systems/osys.sh
+++ operating-systems/osys.sh
@@ -2,13 +2,15 @@
 
 echo "Operating SYStem CLI"
 
+SCRIPT_DIR=${BASH_SOURCE%/*}
 OSYS_WORKING_DIR="$HOME/.osys"
 OSYS_OS_LIST="$OSYS_WORKING_DIR/os.list"
+OSYS_TEMPLATE_DIR="$SCRIPT_DIR/src/templates/"
 
 function init(){
   if [ ! -d "$OSYS_WORKING_DIR" ]; then
     echo "OSYS appears to be running for the first time..."
-   
+
     mkdir "$OSYS_WORKING_DIR"
     touch "$OSYS_OS_LIST"
   fi
@@ -26,58 +28,36 @@ case "$1" in
               HTML_ANCHORS_LIST="$(curl -sL $CENTOS_MIRROR | xmllint --html --xpath //a -)"
               DELIMITER="</a>"
 
-              s=$HTML_ANCHORS_LIST$DELIMITER
-              cat > $OSYS_OS_LIST << EOL
-{
-  centos: [{
-    version: $CENTOS_VERSION,
-    isos: [{
-      minimal: {
-        url: "URL", 
-        sha1: "SHA1",
-        sha256: "SHA256"
-      }},{
-      dvd: {
-        url: "URL",
-        sha1: "SHA1",
-        sha256: "SHA256"
-      }},{
-      everything: {
-        url: "URL",
-        sha1: "SHA1",
-        sha256: "SHA256"
-      }},{
-      net: {
-        url: "URL",
-        sha1: "SHA1",
-        sha256: "SHA256"
-      }},{
-      live-kde: {
-        url: "URL",
-        sha1: "SHA1",
-        sha256: "SHA256"
-      }},{
-      live-gnome: {
-        url: "URL",
-        sha1: "SHA1"
-        sha56: "SHA256"
-      }}]
-  }]
-}
-EOL
+              cp $OSYS_TEMPLATE_DIR/centos_template.json $OSYS_OS_LIST
 
+              sed "s/CENTOS_VERSION/$CENTOS_VERSION/g" $OSYS_OS_LIST > $OSYS_OS_LIST.bak
+              mv $OSYS_OS_LIST.bak $OSYS_OS_LIST
+
+              s=$HTML_ANCHORS_LIST$DELIMITER
               while [[ $s ]]; do
                 CURRENT_ANCHOR=$(echo "${s%%"$DELIMITER"*}" | cut -d'>' -f2)
-                if $(echo $CURRENT_ANCHOR | grep -Eiq "^centos.*iso$|^sha.*txt$"); then  
+                if $(echo $CURRENT_ANCHOR | grep -Eiq "^centos.*iso$|^sha.*txt$"); then
+
                   echo "Adding link"
-                  echo "$CENTOS_MIRROR/$CURRENT_ANCHOR" >> $OSYS_OS_LIST
+                  FILETYPE=$(echo $CURRENT_ANCHOR | cut -d'-' -f4 | tr '[:upper:]' '[:lower:]')
+
+                  if $(echo $FILETYPE | grep -Eiq "^[^sha]"); then
+                    ISOTYPE_URL="${FILETYPE}_url"
+                    sed "s@$ISOTYPE_URL@$CENTOS_MIRROR/$CURRENT_ANCHOR@g" $OSYS_OS_LIST > $OSYS_OS_LIST.bak
+                    mv $OSYS_OS_LIST.bak $OSYS_OS_LIST
+                  else
+                    SHA_LIST="$(curl -sL $CENTOS_MIRROR/$FILETYPE)"
+                    echo $SHA_LIST
+                  fi
+
+
                 fi
                 s=${s#*"$DELIMITER"};
               done;
             ;;
           *)
               cat "$OSYS_OS_LIST"
-              echo "" 
+              echo ""
               echo "To update this list run ./osys list update"
             ;;
         esac
